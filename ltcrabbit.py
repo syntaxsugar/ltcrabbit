cf36b9210c47eb87961aba10371061585121daec
Jaromir Fojtu <jaromir.fojtu@gmail.com>
2014-08-03 22:28:02 +0200

    Add __repr__

--- ltcrabbit.py
+++ ltcrabbit.py
@@ -3,15 +3,25 @@ __version__ = "0.1dev"
 
 import requests
 
-BASE_URL = "https://www.ltcrabbit.com/index.php?page=api"
 
 class LtcRabbit(object):
+    """Main class to access LTCRabbit.com API.
+    """
+
+    DEFAULT_ENDPOINT = "https://www.ltcrabbit.com/index.php?page=api"
 
     appname = "PyLtcRabbit"
     appversion = __version__
 
-    def __init__(self, api_key):
+    def __init__(self, api_key=None):
+        if not api_key:
+            raise RuntimeError("No API key provided.")
+
         self.api_key = api_key
+        self.url = self.DEFAULT_ENDPOINT
+
+    def __repr__(self):
+        return "Connection(%r)" % self.api_key
 
     def call(self, action, **kwargs):
         payload = kwargs
@@ -20,7 +30,7 @@ class LtcRabbit(object):
         payload['appversion'] = self.appversion
         payload['action'] = action
 
-        r = requests.get(BASE_URL, params=payload)
+        r = requests.get(self.url, params=payload)
         pprint.pprint(r.url)
 
         return r.json()
