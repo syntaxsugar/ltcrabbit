823ee1ed1f185b64386b0ba616f5e9f798846aba
Jaromir Fojtu <jaromir.fojtu@gmail.com>
2014-08-15 14:39:40 +0200

    Release v0.2.0

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Requirements
 ============
 
 * Python 2.6 or above
-* `Requests`_ library
+* `requests`_ library
+* `docopt`_ library
+* `prettytable`_ library
 
 Usage
 =====
+
+    ltcrabbit-cli worker list --apikey=<your_api_key>
--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ except ImportError:
 
 setup(
     name='ltcrabbit',
-    version='0.1.3',
+    version='0.2.0',
     author='Jaromir Fojtu',
     author_email='jaromir.fojtu@gmail.com',
     url='https://github.com/syntaxsugar/ltcrabbit',
     py_modules=['ltcrabbit'],
+    scripts=['bin/ltcrabbit-cli'],
     license='BSD',
     description='LTCRabbit.com API wrapper',
     long_description=open('README.md').read(),
-    install_requires=['requests'],
+    install_requires=['requests', 'prettytable', 'docopt'],
 )
